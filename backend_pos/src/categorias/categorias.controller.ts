3bc06b0663184fa7dc1f775f805920d4b856e0ff
AlanAltamiranoH1504 <altamiranohernandezalan@gmail.com>
2025-08-26 20:58:47 -0600

    feat(backend): add pipe for validating id parameter in CategoriesController

--- backend_pos/src/categorias/categorias.controller.ts
+++ backend_pos/src/categorias/categorias.controller.ts
@@ -1,34 +1,36 @@
 import {Controller, Get, Post, Body, Patch, Param, Delete, ParseIntPipe, BadRequestException} from '@nestjs/common';
-import { CategoriasService } from './categorias.service';
-import { CreateCategoriaDto } from './dto/create-categoria.dto';
-import { UpdateCategoriaDto } from './dto/update-categoria.dto';
+import {CategoriasService} from './categorias.service';
+import {CreateCategoriaDto} from './dto/create-categoria.dto';
+import {UpdateCategoriaDto} from './dto/update-categoria.dto';
+import {IdValidationPipe} from "../common/pipes/id-validation/id-validation.pipe";
 
 @Controller('categorias')
 export class CategoriasController {
-  constructor(private readonly categoriasService: CategoriasService) {}
+    constructor(private readonly categoriasService: CategoriasService) {
+    }
 
-  @Post()
-  create(@Body() createCategoriaDto: CreateCategoriaDto) {
-    return this.categoriasService.create(createCategoriaDto);
-  }
+    @Post()
+    create(@Body() createCategoriaDto: CreateCategoriaDto) {
+        return this.categoriasService.create(createCategoriaDto);
+    }
 
-  @Get()
-  findAll() {
-    return this.categoriasService.findAll();
-  }
+    @Get()
+    findAll() {
+        return this.categoriasService.findAll();
+    }
 
-  @Get(':id')
-  findOne(@Param('id', new ParseIntPipe({exceptionFactory: () => new BadRequestException("Id no valido")})) id: string) {
-    return this.categoriasService.findOne(+id);
-  }
+    @Get(':id')
+    findOne(@Param('id', IdValidationPipe) id: string) {
+        return this.categoriasService.findOne(+id);
+    }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateCategoriaDto: UpdateCategoriaDto) {
-    return this.categoriasService.update(+id, updateCategoriaDto);
-  }
+    @Patch(':id')
+    update(@Param('id', IdValidationPipe) id: string, @Body() updateCategoriaDto: UpdateCategoriaDto) {
+        return this.categoriasService.update(+id, updateCategoriaDto);
+    }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.categoriasService.remove(+id);
-  }
+    @Delete(':id')
+    remove(@Param('id', IdValidationPipe) id: string) {
+        return this.categoriasService.remove(+id);
+    }
 }
--- backend_pos/src/categorias/dto/update-categoria.dto.ts
+++ backend_pos/src/categorias/dto/update-categoria.dto.ts
@@ -1,4 +1,9 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateCategoriaDto } from './create-categoria.dto';
+import {IsNotEmpty, IsString} from "class-validator";
 
-export class UpdateCategoriaDto extends PartialType(CreateCategoriaDto) {}
+export class UpdateCategoriaDto extends PartialType(CreateCategoriaDto) {
+    @IsString({message: "El nombre de la categoria debe ser una cadena de texto"})
+    @IsNotEmpty({message: "El nombre de la categoria es obligatorio"})
+    nombre: string;
+}
